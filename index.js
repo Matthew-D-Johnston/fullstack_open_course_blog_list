59174308d083652e38c56aab1647c0b02835c6e4
Matthew Johnston <mattdjohnston92@gmail.com>
2022-11-18 18:10:46 -0400

    Add updated project files.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
 
+const mongoURL = process.env.MONGODB_URI;
+mongoose.connect(mongoURL);
 
-
-app.get('/api/blogs', (request, response) => {
-  // code to retrieve all blogs here
-});
-
-app.post('/api/blogs', (request, response) => {
-  // code to create a blog post here
-});
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
